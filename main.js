4a5bce869781e7fa02f3850e958954a53ffdd75e
natchez <natchezzz@hotmail.com>
2021-01-05 11:20:09 +0100

    fin'

---
+++ main.js
@@ -0,0 +1,74 @@
+        // EXO1
+// let a = 1
+// let b = 2
+// let c = 3
+// let temp
+// temp = a
+// a = b
+// b = c
+// c = temp
+// console.log(a,b,c)
+        // console.log('...........................')
+//         // EXO2
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// console.log(a,b,c,d)
+// let temp
+// let temp1
+// temp = a
+// temp1 = b
+// a = d
+// b = c
+// c = temp1
+// d = temp
+// console.log(a,b,c,d)
+        // console.log('...........................')
+        // EXO3
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// console.log(a,b,c,d)
+// let temp
+// let temp1
+// temp = a
+// temp1 = 
+// a = b
+// b = c
+// c = d
+// d = temp
+// console.log(a,b,c,d)
+        // console.log('...........................')
+        // EXO4
+// let tableau = [1, 2, 3, 4]
+// let temp
+// console.log(tableau)
+// temp = tableau[0]
+// tableau[0] = tableau[3]
+// tableau[3] = temp
+// temp = tableau[1]
+// tableau[1] =tableau[2]
+// tableau[2] =temp
+// console.log(tableau)
+        // console.log('...........................')
+        // EXO5
+let a = 1
+let b = 2
+let c = 3   
+let d = 4   
+let e = 5   
+let f = 6      
+console.log(a, b, c, d, e, f)
+let temp
+let temp1
+temp = c
+c = a
+a = d
+d = b
+b = e
+e = f 
+f = temp
+console.log(a, b, c, d, e, f)
+console.log ('4 5 1 2 6 3')
